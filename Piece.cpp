d509572e4427467855af3a4e78f4c0b9c134c440
gaetan <gaetan.florio@polymtl.ca>
2023-04-04 00:04:43 -0400

    update piece

--- Piece.cpp
+++ Piece.cpp
@@ -1,14 +1,13 @@
 #include "Piece.h"
-#include <SFML/Graphics.hpp>
-#include <iostream>
-using namespace sf;
 
-Piece::Piece(int id, int x, int y)
-{
-	first = true;
-	type = id;
-	pos.x = x;
-	pos.y = y;
+Piece::Piece(const Coord &pos, Colour color) : pos_(pos), color_(color) {}
+
+bool Piece::move(const std::vector<Piece *> &board, const Coord &pos) {
+    if (isLegalMove(board, pos)) {
+        pos_ = pos;
+        return true;
+    }
+    return false;
 }
 
 void Piece::Move(const std::vector<Piece*>* board)
@@ -124,6 +123,34 @@ void Piece::Set_Position(int box)
 std::vector<int> Piece::get_Move() const
 {
 	return move;
+}
+
+bool King::move(const std::vector<Piece *> &board, const Coord &pos) {
+    if (isLegalMove(board, pos)) {
+        if (pos.x == pos_.x + 2 && pos.y == pos_.y) {
+            auto tower = dynamic_cast<Tower *>(board[pos.y * 8 + pos.x + 3]);
+            tower->move(board, {pos.x - 1, pos.y});
+        }
+        if (pos.x == pos_.x - 2 && pos.y == pos_.y) {
+            auto tower = dynamic_cast<Tower *>(board[pos.y * 8 + pos.x - 4]);
+            tower->move(board, {pos.x + 1, pos.y});
+        }
+        pos_ = pos;
+        first_ = false;
+        return true;
+    }
+    return false;
+}
+
+bool King::isCheck(const std::vector<Piece *> &board) {
+    for (int i = 0; i < 8; i++)
+        for (int j = 0; j < 8; j++) {
+            if (board[i * 8 + j] == nullptr)
+                continue;
+            if (board[i * 8 + j]->isLegalMove(board, pos_) && board[i * 8 + j]->getColor() != color_)
+                return true;
+        }
+    return false;
 }
 
 bool Piece::Check_Move(const std::vector<Piece*>* board, int x, int y, bool eat, bool need_eat) // ajouter verification echec a découverte 
@@ -262,4 +289,44 @@ bool Piece::Check_Chess(const std::vector<Piece*>* board, int box)
 		}
 	}
 	return true;
+}
+
+bool Pawn::move(const std::vector<Piece *> &board, const Coord &pos) {
+    if (Piece::move(board, pos)) {
+        if (pos.x == pos_.x + 1 || pos.x == pos_.x - 1) {
+            auto pawn = dynamic_cast<Pawn *>(board[pos_.y * 8 + pos.x]);
+            if (pawn != nullptr && board[pos.y * 8 + pos.x] == nullptr) {
+                if (pawn->first_ == 1)
+                    pawn->isAlive_ = false;
+            }
+        }
+        if (pos.x == pos_.x +2 || pos.x == pos_.x - 2)
+            first_ = 1;
+        first_ = (first_ == 1 | first_ == -1) ? -1 : 0;
+        return true;
+    }
+    return false;
+}
+
+
+Knight::Knight(const Coord &pos, Colour color) : Piece(pos, color) {}
+
+bool Knight::isLegalMove(const std::vector<Piece *> &board, Coord pos) {
+    if (pos.x > 7 || pos.x < 0 || pos.y > 7 || pos.y < 0) {
+        return false;
+    }
+    Piece *piece = board[pos.y * 8 + pos.x];
+    if (piece != nullptr && piece->getColor() == color_) {
+        return false;
+    }
+    if (pos.x == pos_.x && pos.y == pos_.y) {
+        return false;
+    }
+    if (abs(pos.x - pos_.x) == 2 && abs(pos.y - pos_.y) == 1) {
+        return true;
+    }
+    if (abs(pos.x - pos_.x) == 1 && abs(pos.y - pos_.y) == 2) {
+        return true;
+    }
+    return false;
 }
--- Piece.h
+++ Piece.h
@@ -1,28 +1,115 @@
-#ifndef _PIECE_H
-#define _PIECE_H
-#include <SFML/Graphics.hpp>
-struct Pair
-{
-	int x, y;
+#ifndef PIECE_H
+#define PIECE_H
+
+#include <vector>
+
+struct Coord {
+    int x, y;
+
+    Coord &operator=(const Coord &b) = default;
 };
-class Piece
-{
+
+bool operator==(const Coord &a, const Coord &b) {
+    return a.x == b.x && a.y == b.y;
+}
+
+
+enum class Colour {
+    white,
+    black
+};
+
+
+class Piece {
 public:
-	Piece(int id, int x, int y);
-	void Move(const std::vector<Piece*>* board);
-	int Type()const;
-	int Pos() const;
-	void Promote(int _type);
-	void Set_Position(int box);
-	std::vector<int> get_Move() const;
-	bool Check_Chess(const std::vector<Piece*>* board, int box = -1);
+    Piece(const Coord &pos, Colour color);
+
+    virtual bool isLegalMove(const std::vector<Piece *> &board, Coord pos) = 0;
+
+    virtual bool move(const std::vector<Piece *> &board, const Coord &pos);
+
+    [[nodiscard]] Colour getColor() const { return color_; }
+
+    [[nodiscard]] bool isAlive() const { return isAlive_; }
+
+//    [[nodiscard]] Coord Pos() const;
+
+//    void setPosition(int box);
+
+protected:
+    inline static Coord *whiteKingPos = nullptr;
+    inline static Coord *blackKingPos = nullptr;
+//	bool Check_Move(const std::vector<Piece*>* board, int x, int y, bool eat = true, bool need_eat = false);
+//	int Check_Move(int p, const std::vector<Piece*>* board, int x, int y);
+    Coord pos_;
+    Colour color_;
+    bool isAlive_ = true;
+};
+
+
+class Pawn : virtual public Piece {
+public:
+    Pawn(const Coord &pos, Colour color);
+
+    bool move(const std::vector<Piece *> &board, const Coord &pos) override;
+
+    bool isLegalMove(const std::vector<Piece *> &board, Coord pos) override;
 
 private:
-	bool Check_Move(const std::vector<Piece*>* board, int x, int y, bool eat = true, bool need_eat = false);
-	int Check_Move(int p, const std::vector<Piece*>* board, int x, int y);
-	Pair pos;
-	int type;
-	bool first;
-	std::vector<int> move;
+    int first_ = 0;
 };
-#endif // !PIECE
+
+class Knight : virtual public Piece {
+public:
+    Knight(const Coord &pos, Colour color);
+
+    bool isLegalMove(const std::vector<Piece *> &board, Coord pos) override;
+
+};
+
+class Tower : virtual public Piece {
+public:
+    Tower(const Coord &pos, Colour color);
+
+    [[nodiscard]] bool getFirst() const;
+
+    bool move(const std::vector<Piece *> &board, const Coord &pos) override;
+
+    bool isLegalMove(const std::vector<Piece *> &board, Coord pos) override;
+
+private:
+    bool first_ = true;
+};
+
+
+class Bishop : virtual public Piece {
+public:
+    Bishop(const Coord &pos, Colour color);
+
+    bool isLegalMove(const std::vector<Piece *> &board, Coord pos) override;
+};
+
+
+class Queen : public Tower, Bishop {
+public:
+    Queen(const Coord &pos, Colour color);
+
+    bool isLegalMove(const std::vector<Piece *> &board, Coord pos) override;
+};
+
+class King : virtual public Piece {
+public:
+    King(const Coord &pos, Colour color);
+
+    bool move(const std::vector<Piece *> &board, const Coord &pos) override;
+
+    bool isLegalMove(const std::vector<Piece *> &board, Coord pos) override;
+
+    bool isCheck(const std::vector<Piece *> &board);
+
+private:
+    bool first_ = true;
+};
+
+
+#endif // PIECE_H
